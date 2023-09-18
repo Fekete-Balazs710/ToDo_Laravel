1d48f24bc8747ea7b6d4df58271bb48e776868cf
Fekete Balazs <balazs.fekete@stud.ubbcluj.ro>
2023-09-18 17:38:38 +0300

    Corrected ITL-9 code based on review

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Spatie\QueryBuilder\QueryBuilder;
 use App\Http\Requests\{TodoStorePostRequest, TodoUpdateRequest};
 use Illuminate\Http\Request;
 use App\Models\Todo;
@@ -13,39 +14,21 @@ use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class TodoController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
-        $sortBy = $request->query('sort_by', 'title');
-        $sortOrder = $request->query('sort_order', 'asc');
-        $searchQuery = $request->query('search_query', '');
-
-        $validSortColumns = ['title', 'description', 'date', 'priority'];
-        if (!in_array($sortBy, $validSortColumns) || !in_array($sortOrder, ['asc', 'desc'])) {
-            return response()->json(['error' => 'Invalid sort parameters'], 400);
-        }
-
-        $query = Todo::query();
-
-        if ($sortBy === 'priority') {
-            $priorityMap = ['low' => 1, 'medium' => 2, 'high' => 3];
-
-            if ($sortOrder !== 'asc') {
-                $priorityMap = array_reverse($priorityMap);
-            }
-            $priorityOrder = implode("', '", array_keys($priorityMap));
-            $query->orderByRaw("FIELD(priority, '$priorityOrder')");
-        } else {
-            $query->orderBy($sortBy, $sortOrder);
-        }
-
-        if (!empty($searchQuery)) {
-            $query->where(function ($subQuery) use ($searchQuery) {
-                $subQuery->where('title', 'like', "%$searchQuery%")
-                    ->orWhere('description', 'like', "%$searchQuery%");
-            });
-        }
-
-        $todos = $query->get();
+        $todos = QueryBuilder::for(Todo::class)
+            ->defaultSort('date')
+            ->allowedSorts([
+                'title',
+                'description',
+                'priority',
+                'date',
+            ])
+            ->allowedFilters([
+                'title',
+                'description',
+            ])
+            ->get();
 
         return response()->json(['todos' => $todos]);
     }
